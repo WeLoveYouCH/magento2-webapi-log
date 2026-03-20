161ec732cdd95da47bd4edafe39a1ada00dd7534
Philippe Aellig <philippe@aelligs.ch>
2026-03-20 15:21:48 +0100

    Refactor TransportBuilder to add email address handling methods and improve email processing
    - Implemented methods to add CC, BCC, Reply-To, and From addresses.
    - Enhanced email message preparation with better handling for attachments and templates.
    - Improved dependency handling and added missing imports to replace deprecated Laminas MIME classes.

--- Model/Mail/Template/TransportBuilder.php
+++ Model/Mail/Template/TransportBuilder.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace VladFlonta\WebApiLog\Model\Mail\Template;
 
-use Laminas\Mime\Part;
+use Magento\Framework\App\TemplateTypesInterface;
+use Magento\Framework\Exception\MailException;
+use Magento\Framework\Mail\Exception\InvalidArgumentException;
+use Magento\Framework\Mail\MimeInterface;
+use Magento\Framework\Phrase;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\Multipart\MixedPart;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\HTTP\Mime;
 use Magento\Framework\Mail\AddressConverter;
@@ -17,6 +23,7 @@ use Magento\Framework\Mail\Template\FactoryInterface;
 use Magento\Framework\Mail\Template\SenderResolverInterface;
 use Magento\Framework\Mail\TransportInterfaceFactory;
 use Magento\Framework\ObjectManagerInterface;
+// https://magento.stackexchange.com/questions/376823/magento-2-4-8-how-to-add-email-attachments-laminas-mime-library-removed
 
 class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
 {
@@ -30,6 +37,12 @@ class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
     private MimePartInterfaceFactory $mimePartInterfaceFactory;
     private AddressConverter $addressConverter;
 
+    /**
+     * Param that used for storing all message data until it will be used
+     *
+     * @var array
+     */
+    private $messageData = [];
     /**
      * TransportBuilder constructor
      *
@@ -80,11 +93,113 @@ class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
     }
 
     /**
+     * Add cc address
+     *
+     * @param array|string $address
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function addCc($address, $name = '')
+    {
+        $this->addAddressByType('cc', $address, $name);
+
+        return $this;
+    }
+
+    /**
+     * Add to address
+     *
+     * @param array|string $address
+     * @param string $name
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    public function addTo($address, $name = '')
+    {
+        $this->addAddressByType('to', $address, $name);
+
+        return $this;
+    }
+
+    /**
+     * Add bcc address
+     *
+     * @param array|string $address
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    public function addBcc($address)
+    {
+        $this->addAddressByType('bcc', $address);
+
+        return $this;
+    }
+
+    /**
+     * Set Reply-To Header
+     *
+     * @param string $email
+     * @param string|null $name
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     */
+    public function setReplyTo($email, $name = null)
+    {
+        $this->addAddressByType('replyTo', $email, $name);
+
+        return $this;
+    }
+
+
+    /**
+     * Set mail from address
+     *
+     * @param string|array $from
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     * @see setFromByScope()
+     *
+     * @deprecated 102.0.1 This function sets the from address but does not provide
+     * a way of setting the correct from addresses based on the scope.
+     */
+    public function setFrom($from)
+    {
+        return $this->setFromByScope($from);
+    }
+
+    /**
+     * Set mail from address by scopeId
+     *
+     * @param string|array $from
+     * @param string|int $scopeId
+     *
+     * @return $this
+     * @throws InvalidArgumentException
+     * @throws MailException
+     * @since 102.0.1
+     */
+    public function setFromByScope($from, $scopeId = null)
+    {
+        $result = $this->_senderResolver->resolve($from, $scopeId);
+        $this->addAddressByType('from', $result['email'], $result['name']);
+
+        return $this;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function reset(): TransportBuilder
     {
-        parent::reset();
+        $this->messageData = [];
+        $this->templateIdentifier = null;
+        $this->templateVars = null;
+        $this->templateOptions = null;
         $this->attachments = [];
 
         return $this;
@@ -103,7 +218,7 @@ class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
         string $fileName,
         ?string $fileType = Mime::TYPE_OCTETSTREAM
     ): TransportBuilder {
-        $attachmentPart = new Part();
+        $attachmentPart = new DataPart();
         // phpcs:ignore Magento2.Functions.DiscouragedFunction
         $attachmentPart->setContent(base64_decode($content))
             ->setType($fileType)
@@ -121,29 +236,82 @@ class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
      */
     protected function prepareMessage(): TransportBuilder
     {
-        parent::prepareMessage();
+        $template = $this->getTemplate();
+        $content = $template->processTemplate();
 
-        $body = $this->message->getBody();
-        if (method_exists($body, 'getParts')) {
-            $parts = $body->getParts();
-        } else {
-            $parts = [$body];
+        switch ($template->getType()) {
+            case TemplateTypesInterface::TYPE_TEXT:
+                $partType = MimeInterface::TYPE_TEXT;
+                break;
+
+            case TemplateTypesInterface::TYPE_HTML:
+                $partType = MimeInterface::TYPE_HTML;
+                break;
+
+            default:
+                throw new LocalizedException(
+                    new Phrase('Unknown template type')
+                );
         }
-        $parts = array_merge($parts, $this->attachments);
-        $encoding = $this->message->getEncoding();
-        $messageData = [
-            'encoding' => $encoding ?: 'utf-8',
-            'subject' => $this->message->getSubject(),
-            'sender' => $this->message->getSender(),
-            'to' => $this->message->getTo(),
-            'replyTo' => $this->message->getReplyTo(),
-            'from' => $this->message->getFrom(),
-            'cc' => $this->message->getCc(),
-            'bcc' => $this->message->getBcc(),
-        ];
-        $messageData['body'] = $this->mimeMessageInterfaceFactory->create(['parts' => $parts]);
-        $this->message = $this->emailMessageInterfaceFactory->create($messageData);
+
+        /** @var \Magento\Framework\Mail\MimePartInterface $mimePart */
+        $mimePart = $this->mimePartInterfaceFactory->create(
+            [
+                'content' => $content,
+                'type' => $partType
+            ]
+        );
+        $this->messageData['encoding'] = $mimePart->getCharset();
+        $this->messageData['body'] = $this->mimeMessageInterfaceFactory->create(
+            ['parts' => [$mimePart]]
+        );
+
+        $this->messageData['subject'] = html_entity_decode(
+            (string)$template->getSubject(),
+            ENT_QUOTES
+        );
+
+        $this->message = $this->emailMessageInterfaceFactory->create($this->messageData);
+
+
+
+
+        if (count($this->attachments)) {
+
+            $this->messageData['body']->getMimeMessage()->setBody(
+                new MixedPart($this->messageData['body']->getMimeMessage()->getBody(), ...$this->attachments)
+            );
+        }
 
         return $this;
+    }
+
+
+
+    /**
+     * Handles possible incoming types of email (string or array)
+     *
+     * @param string $addressType
+     * @param string|array $email
+     * @param string|null $name
+     *
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    private function addAddressByType(string $addressType, $email, ?string $name = null): void
+    {
+        if (is_string($email)) {
+            $this->messageData[$addressType][] = $this->addressConverter->convert($email, $name);
+            return;
+        }
+        $convertedAddressArray = $this->addressConverter->convertMany($email);
+        if (isset($this->messageData[$addressType])) {
+            $this->messageData[$addressType] = array_merge(
+                $this->messageData[$addressType],
+                $convertedAddressArray
+            );
+        } else {
+            $this->messageData[$addressType] = $convertedAddressArray;
+        }
     }
 }
